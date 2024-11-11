8a7fe8e32c7e205e7f3ee17a4e7e97094a5e5a83
Kamalallahabi <83436046+Kamalallahabi@users.noreply.github.com>
2024-11-11 13:33:33 +0300

    Update dai.js

--- dai.js
+++ dai.js
@@ -1,14 +1,14 @@
 $("<style>.nosel.d-flex.room.rk0he3mfe41 .label-primary {  letter-spacing: 1px;\n  -webkit-text-fill-color: #0c4f4a;\n\t\n  border-radius: 15px 0px 15px 0px !important;\n  height: 27px;\n  margin-top: 60px !important;\n\n  margin-bottom: 0px !important;\n  background-color: #d9534f00;\n  text-align: left!important;\n  margin-left: 40px!important;\ntext-shadow: 0px 0px 0px #fff, 0 0 25px #fff, 0 0 5px #fff!important;\nbackground-image: url(https://up6.cc/2023/03/167959927008681.png);\n\tborder-radius: 6px 6px 6px 6px;\n\tbackground-size: cover;}\n\n.nosel.d-flex.room.rk0he3mfe41 .corner.fa.fa-user.label.uc.fa-microphone.label-danger {\n  letter-spacing: 1px;\n  -webkit-text-fill-color: #0c4f4a;\n\n\n\n\t\n  border-radius: 15px 0px 15px 0px !important;\n  height: 27px;\n  margin-top: 60px !important;\n  margin-bottom: 0px !important;\n  background-color: #d9534f00;\n  text-align: left!important;\n  margin-left: 40px!important;\ntext-shadow: 0px 0px 0px #fff, 0 0 25px #fff, 0 0 5px #fff!important;\nbackground-image: url(https://up6.cc/2023/03/167959927008681.png);\n\tborder-radius: 6px 6px 6px 6px;\n\tbackground-size: cover;\n}\n.nosel.d-flex.room.rk0he3mfe41.bord {border-inline: 0px solid #4c121e !important;}\n.nosel.d-flex.room.rk0he3mfe41 .dots.mini.u-msg {\nbackground-image: url(https://up6.cc/2023/03/167959927008681.png);\n\tborder-radius: 6px 6px 6px 6px;\n\tbackground-size: cover;\n    text-align: center;\n    margin-top: 60px !important;\n\tmargin-left: 11px;\n\t-webkit-text-fill-color: #000;\n}\n.nosel.d-flex.room.rk0he3mfe41 {\n    background-image: url(https://up6.cc/2024/05/171562793731772.gif);\n    background-size: cover;\n    border-radius: 15px 0px 15px 0px;\n    display: inline-block;\n}\n.nosel.d-flex.room.rk0he3mfe41 .fitimg.u-pic.borderg {\n  border-radius: 100px;\n  border: 2px solid #cc8321;\n  letter-spacing: 1px;\n  background-size: 108% 108%;\n  -webkit-text-fill-color: white;\n  animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;\n  min-width: 62px !important;\n  width: 62px !important;\n  min-height: 60px !important;\n  max-height: 60px !important;\n  display: none;\n}\n.nosel.d-flex.room.rk0he3mfe41 .u-topic.dots {\n  text-transform: uppercase;\n  -webkit-text-fill-color: #e8a64d00;\n  animation: textclip 2s linear infinite;\n  display: inline-block;\n  font-size: 22px !important;\n  -webkit-background-clip: text;\n  color: transparent !important;\n  margin-top: 10px;\n  display: none;\n}\n@keyframes textclip {to {background-position: 200% center;}}</style>").insertBefore("body");
 $("style").last().append("\n\n\t\n\n\n#chats.sudia {\nbackground-image: url(https://up6.cc/2024/05/171562793730591.jpg);\n\n\tbackground-size: 100%;\n}\n\n");
-var stop = false;
+var myroom = false;
 setInterval(function() {
     if (myroom == "k0he3mfe41") {
-        if (stop == false) {
-            stop = true;
+        if (myroom == false) {
+            myroom = true;
             $('#chats').addClass('sudia');
         }
-    } else if (stop == true) {
-        stop = false;
+    } else if (myroom == true) {
+        myroom = false;
         $("#chats").removeClass("sudia");
     }
 }, 0x14);
