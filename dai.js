af7318ae6b65ab35b2673ac942dc28180e921f13
Kamalallahabi <83436046+Kamalallahabi@users.noreply.github.com>
2024-11-11 14:37:34 +0300

    Update dai.js

--- dai.js
+++ dai.js
@@ -1,30 +1,24 @@
-$("<style>")
-  .text(`
+// إضافة CSS للصفحة مباشرةً
+$("<style>").text(`
     .nosel.d-flex.room.rk0he3mfe41 .label-primary {
         letter-spacing: 1px;
-        -webkit-text-fill-color: #0c4f4a;
+        color: #0c4f4a;
         border-radius: 15px 0px 15px 0px !important;
         height: 27px;
         margin-top: 60px !important;
-        margin-bottom: 0px !important;
         background-color: #d9534f00;
-        text-align: left!important;
-        margin-left: 40px!important;
         text-shadow: 0px 0px 0px #fff, 0 0 25px #fff, 0 0 5px #fff!important;
         background-image: url(https://up6.cc/2023/03/167959927008681.png);
         background-size: cover;
     }
-    
+
     .nosel.d-flex.room.rk0he3mfe41 .corner.fa.fa-user.label.uc.fa-microphone.label-danger {
         letter-spacing: 1px;
-        -webkit-text-fill-color: #0c4f4a;
+        color: #0c4f4a;
         border-radius: 15px 0px 15px 0px !important;
         height: 27px;
         margin-top: 60px !important;
-        margin-bottom: 0px !important;
         background-color: #d9534f00;
-        text-align: left!important;
-        margin-left: 40px!important;
         text-shadow: 0px 0px 0px #fff, 0 0 25px #fff, 0 0 5px #fff!important;
         background-image: url(https://up6.cc/2023/03/167959927008681.png);
         background-size: cover;
@@ -41,37 +35,32 @@ $("<style>")
         text-align: center;
         margin-top: 60px !important;
         margin-left: 11px;
-        -webkit-text-fill-color: #000;
+        color: #000;
     }
 
     .nosel.d-flex.room.rk0he3mfe41 {
         background-image: url(https://up6.cc/2024/05/171562793731772.gif);
         background-size: cover;
         border-radius: 15px 0px 15px 0px;
-        display: inline-block;
     }
 
     .nosel.d-flex.room.rk0he3mfe41 .fitimg.u-pic.borderg {
         border-radius: 100px;
         border: 2px solid #cc8321;
         background-size: 108% 108%;
-        -webkit-text-fill-color: white;
-        animation: grad 2s cubic-bezier(.445, .05, .55, .95) alternate infinite;
-        min-width: 62px !important;
-        width: 62px !important;
-        min-height: 60px !important;
-        max-height: 60px !important;
+        min-width: 62px;
+        width: 62px;
+        min-height: 60px;
+        max-height: 60px;
         display: none;
     }
 
     .nosel.d-flex.room.rk0he3mfe41 .u-topic.dots {
         text-transform: uppercase;
-        -webkit-text-fill-color: #e8a64d00;
-        animation: textclip 2s linear infinite;
-        font-size: 22px !important;
-        -webkit-background-clip: text;
         color: transparent !important;
-        margin-top: 10px;
+        font-size: 22px;
+        background-clip: text;
+        animation: textclip 2s linear infinite;
         display: none;
     }
 
@@ -80,19 +69,17 @@ $("<style>")
             background-position: 200% center;
         }
     }
+`).appendTo("head");
 
-    #chats.sudia {
-        background-image: url(https://up6.cc/2024/05/171562793730591.jpg);
-        background-size: 100%;
-    }
-  `).appendTo("head");
-
+// تفعيل وإزالة الأنماط عند الانضمام إلى الغرفة
 var myroom = false;
 setInterval(function() {
-    if (!myroom && $("#chats").hasClass("sudia")) {
-        myroom = true;
-        $('#chats').addClass('k0he3mfe41');
-    } else if (myroom && !$("#chats").hasClass("sudia")) {
+    if (myroom === "k0he3mfe41") {
+        if (!myroom) {
+            myroom = true;
+            $('#chats').addClass('k0he3mfe41');
+        }
+    } else if (myroom) {
         myroom = false;
         $("#chats").removeClass("k0he3mfe41");
     }
